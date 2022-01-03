71484746cbfee842ec420eebc58446ffaae06a6f
Anthony Collier <hello@acollier.dev>
2022-01-03 18:55:35 -0500

    :sparkles: Provide functionality for all services

--- routes.go
+++ routes.go
@@ -53,22 +53,16 @@ func getGuildCount(ctx *fiber.Ctx) error {
 }
 
 func getBotListServices(ctx *fiber.Ctx) error {
-	//var topgg *BotListServiceResponse
-	//var botsgg *BotListServiceResponse
-	//var dlspace *BotListServiceResponse
-	//var dbl *BotListServiceResponse
-	//var discords *BotListServiceResponse
-
 	var responses []*BotListServiceResponse
 
 	var wg sync.WaitGroup
-	wg.Add(1)
+	wg.Add(5)
 
 	client := &http.Client{Timeout: time.Second * 30}
 
 	// top.gg
 	go func() {
-		body, reqErr := getBodyFromBotListService(client, "topgg")
+		body, reqErr := getDataFromBotListService(client, "topgg")
 		if reqErr != nil {
 			fiber.NewError(fiber.StatusInternalServerError, reqErr.Error())
 		}
@@ -76,8 +70,78 @@ func getBotListServices(ctx *fiber.Ctx) error {
 		responses = append(responses, &BotListServiceResponse{
 			Id:         services.Get("services.topgg.id").(int64),
 			ShortName:  services.Get("services.topgg.short_name").(string),
+			Url:        services.Get("services.topgg.get_stats_url").(string),
 			GuildCount: int64(body["server_count"].(float64)),
-			Enabled:    services.Get("services.topgg.enabled").(bool),
+		})
+
+		wg.Done()
+	}()
+
+	// discord.bots.gg
+	go func() {
+		body, reqErr := getDataFromBotListService(client, "botsgg")
+		if reqErr != nil {
+			fiber.NewError(fiber.StatusInternalServerError, reqErr.Error())
+		}
+
+		responses = append(responses, &BotListServiceResponse{
+			Id:         services.Get("services.botsgg.id").(int64),
+			ShortName:  services.Get("services.botsgg.short_name").(string),
+			Url:        services.Get("services.botsgg.get_stats_url").(string),
+			GuildCount: int64(body["guildCount"].(float64)),
+		})
+
+		wg.Done()
+	}()
+
+	// discordlist.space
+	go func() {
+		body, reqErr := getDataFromBotListService(client, "dlspace")
+		if reqErr != nil {
+			fiber.NewError(fiber.StatusInternalServerError, reqErr.Error())
+		}
+
+		responses = append(responses, &BotListServiceResponse{
+			Id:         services.Get("services.dlspace.id").(int64),
+			ShortName:  services.Get("services.dlspace.short_name").(string),
+			Url:        services.Get("services.dlspace.get_stats_url").(string),
+			GuildCount: int64(body["serverCount"].(float64)),
+		})
+
+		wg.Done()
+	}()
+
+	// discordbotlist.com
+	go func() {
+		body, reqErr := getDataFromBotListService(client, "dbl")
+		if reqErr != nil {
+			fiber.NewError(fiber.StatusInternalServerError, reqErr.Error())
+		}
+
+		fmt.Printf("%s", body["stats"].(map[string]interface{})["guilds"])
+
+		responses = append(responses, &BotListServiceResponse{
+			Id:         services.Get("services.dbl.id").(int64),
+			ShortName:  services.Get("services.dbl.short_name").(string),
+			Url:        services.Get("services.dbl.get_stats_url").(string),
+			GuildCount: int64(body["stats"].(map[string]interface{})["guilds"].(float64)),
+		})
+
+		wg.Done()
+	}()
+
+	// discords.com
+	go func() {
+		body, reqErr := getDataFromBotListService(client, "discords")
+		if reqErr != nil {
+			fiber.NewError(fiber.StatusInternalServerError, reqErr.Error())
+		}
+
+		responses = append(responses, &BotListServiceResponse{
+			Id:         services.Get("services.discords.id").(int64),
+			ShortName:  services.Get("services.discords.short_name").(string),
+			Url:        services.Get("services.discords.get_stats_url").(string),
+			GuildCount: int64(body["server_count"].(float64)),
 		})
 
 		wg.Done()
